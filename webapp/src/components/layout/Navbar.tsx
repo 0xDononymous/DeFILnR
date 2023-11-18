1c5800feeb126602baf5809446bd9298dcdb596b
cc03668 <cc03668@gmail.com>
2023-11-18 18:07:26 +0300

    Modify Navbar

--- webapp/src/components/layout/Navbar.tsx
+++ webapp/src/components/layout/Navbar.tsx
@@ -11,19 +11,22 @@ export default function Navbar() {
   return (
     <div className="flex flex-row justify-between items-center w-full px-8 py-4 border-b-[1px] border-darkgrey shadow-md">
       <Link href="/" className="text-xl text-white font-mono">
-        <div >
-          Autonomous Airdrop Example
+      <div >
+          DeFi L&R
         </div>
       </Link>
       <div className="flex flex-row items-center gap-4 sm:gap-8">
         <Link href="https://axiom.xyz">
           Axiom
         </Link>
-        <Link href="https://docs-v2.axiom.xyz/">
-          Docs
+        <Link href="https://gearbox.fi/">
+         GearBox
         </Link>
-        <Link href="https://github.com/axiom-crypto/autonomous-airdrop-example">
-          Github
+        <Link href="https://www.hyperlane.xyz/">
+          HyperLane
+        </Link>
+        <Link href="https://thegraph.com/">
+          The Graph
         </Link>
         <ConnectWallet connected={connected} />
       </div>
