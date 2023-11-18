6d6beaa68661d88df9a73c57f1cc29047c11eb79
cc03668 <cc03668@gmail.com>
2023-11-18 20:41:11 +0300

    gp page

---
+++ webapp/src/app/gbnondegen/page.tsx
@@ -0,0 +1,37 @@
+import Title from "@/components/ui/Title";
+import { Constants } from "@/shared/constants";
+import Link from "next/link";
+import LinkButton from '@/components/ui/LinkButton'
+import ConnectWallet from '@/components/ui/ConnectWallet'
+import { forwardSearchParams } from '@/lib/utils'
+
+
+interface PageProps {
+  params: Params;
+  searchParams: SearchParams;
+}
+
+interface Params {
+  slug: string;
+}
+
+interface SearchParams {
+  [key: string]: string | string[] | undefined;
+}
+
+export default async function Success({ searchParams }: PageProps) {
+  const connected = searchParams?.connected as string ?? "";
+
+
+  return (
+    <>
+      <Title>
+      Hi Degen!!! &#128056;
+      </Title>
+      <div className="text-left">
+        Max Debt: <br />
+        CA Address: <br />
+      </div>  
+    </>
+  )
+}
