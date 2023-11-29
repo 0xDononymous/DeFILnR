24a03ac42c7e77996627949b00ff170160883aab
ryanycw <ryan@ryanycw.dev>
2023-11-30 02:36:04 +0800

    fix: recompile circuit

--- webapp/src/lib/circuit/compile.sh
+++ webapp/src/lib/circuit/compile.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-source .env.local
+source .env
 
 ALCHEMY_KEY=${NEXT_PUBLIC_ALCHEMY_KEY}
 URL="https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_KEY}"
 
-npx axiom compile ./src/components/axiom/circuit/circuit.ts -o ./src/components/axiom/circuit/build.json -p $URL
+npx axiom compile ./src/lib/circuit/circuit.ts -o ./src/lib/circuit/build.json -p $URL
