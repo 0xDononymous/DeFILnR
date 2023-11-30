318350beb13fc56c1b1c3ea176fdabee0ab6a215
ryanycw <ryan@ryanycw.dev>
2023-11-30 12:00:07 +0800

    chore: adjust deposit to make it cheaper

--- contracts/script/Membership.s.sol
+++ contracts/script/Membership.s.sol
@@ -24,7 +24,7 @@ contract MembershipScript is Script {
             QUERY_SCHEMA
         );
 
-        membership.deposit{value: 0.05 ether}();
+        membership.deposit{value: 0.03 ether}();
 
         vm.stopBroadcast();
     }
