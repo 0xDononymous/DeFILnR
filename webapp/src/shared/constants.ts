5876c3b970abf3b7fba738f26266e3568880f65b
ryanycw <ryan@ryanycw.dev>
2023-11-30 12:01:11 +0800

    chore: update constants

--- webapp/src/shared/constants.ts
+++ webapp/src/shared/constants.ts
@@ -3,16 +3,16 @@ export const Constants = Object.freeze({
   EXPLORER_BASE_URL: 'https://explorer.axiom.xyz/v2/goerli/query/',
   UNISWAP_UNIV_ROUTER_GOERLI: '0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'.toLowerCase(),
 
-  AUTO_AIRDROP_ADDR: '0xb8249737191E1e2480C5c48CbE36bd47dD7Ece79',
+  //AUTO_AIRDROP_ADDR: '0xb8249737191E1e2480C5c48CbE36bd47dD7Ece79',
 
   // Swap (address sender, address recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)
   EVENT_SCHEMA:
-    '0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67',
+    '0xfa2baf5d3eb95569f312f22477b246f9d4c50276f1cb3ded8e1aeadcbc07a763',
   ELIGIBLE_BLOCK_HEIGHT: 9000000,
-  GOERLI_MEMBERSHIP_ADDR: '0x61497d2200Bf710F9B62b11c1DC340AED417Ec81',
+  GOERLI_MEMBERSHIP_ADDR: '0x7478F57AeF90F3Bd255f27CdD9fb433E3349782b',
 
   GEARBOX_RPC:
     'https://anvil.gearbox.foundation/forks/bbf427c5-90ba-4dc1-84b2-9296fe1c3f2c',
   manager: '0xf409B587Bd0b9baC4DF791117F0cbdA424c195e1',
-  gater: '0x613B9ccaE524510F454549226673abE9404Ad547',
+  gater: '0x6a84CB4871ea094fe7D2230d2b994fAcc34BFF76',
 })
